0cad262ba0c0f18bc402448aeed710159884c6cc
Len Volk <levolkov@microsoft.com>
2022-04-29 13:56:46 -0400

    update

--- Standard-Deployments/CAF-Landing-Zone-MG/_deployment.ps1
+++ Standard-Deployments/CAF-Landing-Zone-MG/_deployment.ps1
@@ -1,3 +1,7 @@
+
+# Install AZ Cli
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+
 New-AzRoleAssignment -SignInName "lv@lvolk.com" -Scope "/" -RoleDefinitionName "Owner"
 
 # authenticate to the portal
