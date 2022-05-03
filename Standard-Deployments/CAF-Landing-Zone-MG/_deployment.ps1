4ce5dc9d56ced990ed9ca0d3eb23199048c11ab1
Len Volk <levolkov@microsoft.com>
2022-05-03 15:50:10 -0400

    tst

--- Standard-Deployments/CAF-Landing-Zone-MG/_deployment.ps1
+++ Standard-Deployments/CAF-Landing-Zone-MG/_deployment.ps1
@@ -2,6 +2,8 @@
 # Install AZ Cli
 $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
 
+# from AAD properties make sure "Access management for Azure resources" is set to yes
+
 New-AzRoleAssignment -SignInName "lv@lvolk.com" -Scope "/" -RoleDefinitionName "Owner"
 
 #Choose MAG
